7c2392ec5c05e5129ffbbe1cbf43d20f1ff8769b
Jon Geyer <jon@supraset.com>
2025-03-17 10:14:23 -0700

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import partytown from '@astrojs/partytown';
+import partytown from '@astrojs/partytown'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()]
-});
+  site: 'https://supraset.com',
+  integrations: [partytown(), sitemap()]
+})
